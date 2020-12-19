2dc940f0931252cefb3931bf42949e944266368a
jarra <suhrawardi@gmail.com>
2020-12-19 23:26:36 +0100

    Playing around a bit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate serde;
 extern crate serde_xml_rs;
 
 use serde_xml_rs::from_reader;
+use std::collections::HashMap;
+use std::cell::Cell;
 use std::fs::File;
 
 #[derive(Debug, Deserialize)]
@@ -27,7 +29,15 @@ struct Property {
     #[serde(rename = "Nullable",default)]
     pub nullable: bool,
     #[serde(rename = "MaxLength",default)]
-    pub length: u8
+    pub length: u8,
+    #[serde(default)]
+    pub key: Cell<bool>
+}
+
+impl Property {
+    fn make_prim(&self) {
+        self.key.set(true);
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -59,24 +69,22 @@ struct Edmx {
     pub dataservice: DataService
 }
 
-#[derive(Debug)]
-struct OData {
-    entities: Vec<String>
-}
-
-impl From<Edmx> for OData {
-    fn from(e: Edmx) -> Self {
-        Self {
-//          entities: e.dataservice.schema.entities.into_iter().collect()
-            entities: e.dataservice.schema.entities.iter().map(|x| x.name.clone()).collect::<Vec<_>>()
-        }
-    }
-}
-
 fn main() {
     let file = File::open("odata_metadata.xml").unwrap();
     let edmx: Edmx = from_reader(file).unwrap();
-    let odata = OData::from(edmx);
-
-    println!("{:#?}", odata.entities);
+    let mut entities: HashMap<String,HashMap<String,&Property>> = HashMap::new();
+    for entity in edmx.dataservice.schema.entities.iter() {
+        let mut props: HashMap<String,_> = HashMap::new();
+        for prop in entity.props.iter() {
+            props.insert(prop.name.clone(), prop);
+        }
+        for key in &entity.keys {
+            for prop_ref in &key.refs {
+                props.get_mut(&prop_ref.name).unwrap().make_prim();
+                println!("{:#?}", prop_ref.name);
+            }
+        }
+        entities.insert(entity.name.clone(), props);
+    }
+    println!("{:#?}", entities);
 }
